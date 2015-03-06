8c668b2d6aafc7e7ad50782c5f126e3d53ac8b94
Jeff Reichenberg <Jeffrey.Reichenberg@ge.com>
2015-03-06 13:49:14 -0800

    Correct default for open option

--- tasks/grunt-dep-serve.js
+++ tasks/grunt-dep-serve.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
 
     var _defaultOptions = {
         depDir: '/bower_components',
-        open: true
+        open: '/'
     };
 
     function startLocalHttpServer(opts) {

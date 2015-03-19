251da447ee6b60ea9f452f5255a50e119b3e05c4
Jeff Reichenberg <Jeffrey.Reichenberg@ge.com>
2015-03-19 09:52:59 -0700

    Updated to actual tag of http-server

--- verifyGlobalPackages.js
+++ verifyGlobalPackages.js
@@ -5,55 +5,29 @@ var exec = require('child_process').exec;
 var _verifyGlobalPackage = function(pkgName, version, link) {
     var resolvedPkgName = version ? pkgName + "@" + version : pkgName;
     console.log("Verifying " + resolvedPkgName + "...");
-    if (pkgName === 'http-server' && version === "master") {
-        //total hack since we want #master of http-server until new version is released with --proxy option to reroute on 404s
-        exec('npm list -g http-server@0.7.5 --depth=0', function(err, stdout, stderr) {
-            console.log(stdout);
-            if (stdout.indexOf("(empty)") !== -1) {
-                console.log("Installing " + resolvedPkgName + "...");
-                exec('npm install -g git://github.com/nodeapps/http-server.git#master', function(err, stdout, stderr) {
-                    console.log(stdout);
-                    if (link) {
-                        exec('npm link ' + pkgName, function(err, stdout, stderr) {
-                            console.log(stdout);
-                        });
-                    }
-                });
-            }
-            else {
+    exec('npm list -g ' + resolvedPkgName + ' --depth=0', function(err, stdout, stderr) {
+        console.log(stdout);
+        if (stdout.indexOf("(empty)") !== -1) {
+            console.log("Installing " + resolvedPkgName + "...");
+            exec('npm install -g ' + resolvedPkgName, function(err, stdout, stderr) {
+                console.log(stdout);
                 if (link) {
                     exec('npm link ' + pkgName, function(err, stdout, stderr) {
                         console.log(stdout);
                     });
                 }
-            }
-        });
-    }
-    else {
-        exec('npm list -g ' + resolvedPkgName + ' --depth=0', function(err, stdout, stderr) {
-            console.log(stdout);
-            if (stdout.indexOf("(empty)") !== -1) {
-                console.log("Installing " + resolvedPkgName + "...");
-                exec('npm install -g ' + resolvedPkgName, function(err, stdout, stderr) {
+            });
+        }
+        else {
+            if (link) {
+                exec('npm link ' + pkgName, function(err, stdout, stderr) {
                     console.log(stdout);
-                    if (link) {
-                        exec('npm link ' + pkgName, function(err, stdout, stderr) {
-                            console.log(stdout);
-                        });
-                    }
                 });
             }
-            else {
-                if (link) {
-                    exec('npm link ' + pkgName, function(err, stdout, stderr) {
-                        console.log(stdout);
-                    });
-                }
-            }
-        });
-    }
+        }
+    });
 };
 
 
 console.log("Verifying global node packages and installing missing ones...");
-_verifyGlobalPackage('http-server', 'master', /*link*/true);
+_verifyGlobalPackage('http-server', '0.8.x', /*link*/true);
